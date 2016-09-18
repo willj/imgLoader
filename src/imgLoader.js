ac2a1da45a7ba95a649b305ffb8e3c5fcc36f47c
Will <willj@users.noreply.github.com>
2016-09-18 12:25:35 +0100

    Scale in steps of 0.5 for much better antialiasing

--- src/imgLoader.js
+++ src/imgLoader.js
@@ -1,6 +1,6 @@
 function imgLoader(settings){
 
-    var filePicker, results = {};
+    var filePicker, stepCanvas1, stepCtx1, stepCanvas2, stepCtx2, results = {};
 
     if (!settings.inputId || !settings.templates || (settings.templates.length < 1)){
         return console.error("You must specify the id of a file input element and at least one job template");
@@ -30,8 +30,18 @@ function imgLoader(settings){
     function imageLoaded(event){
         var img = event.target;
 
+        stepCanvas1 = document.createElement("canvas");
+        stepCtx1 = stepCanvas1.getContext("2d");
+        stepCanvas2 = document.createElement("canvas");
+        stepCtx2 = stepCanvas2.getContext("2d");
+
         for (var i = 0; i < settings.templates.length; i++){
             resizeImage(img, settings.templates[i]);
+
+            stepCanvas1.width = 0;
+            stepCanvas1.height = 0;
+            stepCanvas2.width = 0;
+            stepCanvas2.height = 0;
         }
 
         if (settings.callback){
@@ -52,9 +62,36 @@ function imgLoader(settings){
         }
 
         var scaledSize = calcDimensions(img.width, img.height, template.width, template.height);
+
+        var tempImg = img;
+
+
+        // if the image is more than 2x of the target size
+        if(scaledSize.width < img.width / 2){
+            var tempWidth = img.width;
+            var tempHeight = img.height;
+
+            do {
+                // step it down by halving it each step
+                // this gives *far* better anti-aliasing
+                tempWidth = tempWidth * 0.5;
+                tempHeight = tempHeight * 0.5;
+
+                stepCanvas2.width = tempWidth;
+                stepCanvas2.height = tempHeight;
+
+                stepCtx2.drawImage(tempImg, 0, 0, tempWidth, tempHeight);
+                tempImg = stepCanvas1;
+
+                stepCanvas1.width = tempWidth;
+                stepCanvas1.height = tempHeight;
+                stepCtx1.drawImage(stepCanvas2, 0, 0, tempWidth, tempHeight);
+            } while (tempWidth >= scaledSize.width * 2);
+        }
+
         results[template.id].canvas.width = scaledSize.width;
         results[template.id].canvas.height = scaledSize.height;
-        results[template.id].ctx.drawImage(img, 0, 0, scaledSize.width, scaledSize.height);
+        results[template.id].ctx.drawImage(tempImg, 0, 0, scaledSize.width, scaledSize.height);
     }
 
     function calcDimensions(origWidth, origHeight, maxWidth, maxHeight){
